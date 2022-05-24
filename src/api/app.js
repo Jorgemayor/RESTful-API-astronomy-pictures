70ece64fa3f0e7cf5604e06bbf3a32b8c980e5e9
Jorgemayor <jorge.e.mayor@gmail.com>
2022-05-24 16:18:53 +0300

    style: improve style

--- src/api/app.js
+++ src/api/app.js
@@ -37,12 +37,12 @@ app.use((req, res, next) => {
 	next(error)
 })
 
-app.use((error, req, res, next) => {
+app.use((error, req, res) => {
 	res.status(error.status || 500)
 	res.json({
 		error: {
 			status: error.status,
-			message: error.message
+			message: error.message,
 		},
 	})
 })
