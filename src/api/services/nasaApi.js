ba7f00611b9858bc18a981c6b0141eec873dc5b6
Jorgemayor <jorge.e.mayor@gmail.com>
2022-05-24 19:16:49 +0300

    feat: add method to get nasa pictures

---
+++ src/api/services/nasaApi.js
@@ -0,0 +1,26 @@
+const axios = require("axios")
+
+const numberOfPictures = 100
+
+const getNasaPictures = async () => {
+	let pictures = []
+	try {
+		const { data } = await axios.get(
+			`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&count=${numberOfPictures}`
+		)
+		for (const picture of data) {
+			const { explanation, hdurl, title, url } = picture
+			pictures.push({
+				explanation: explanation,
+				hdurl: hdurl,
+				title: title,
+				url: url,
+			})
+		}
+	} catch (error) {
+		console.log("Error getting the pictures from the NASA API\n", error)
+	}
+	return pictures
+}
+
+module.exports = { getNasaPictures }
